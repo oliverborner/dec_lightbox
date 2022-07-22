b077e5164d14a19001c503b924e53181a660134d
Oliver Borner <61107801+oliverborner@users.noreply.github.com>
2022-07-22 14:36:58 +0200

    Update lightbox_decrow.ino

--- lightbox_decrow.ino
+++ lightbox_decrow.ino
@@ -35,8 +35,8 @@
   -------------------------------------------------------------------
 
   Developed by Oliver Borner, 2022
+  MIT LICENCE
   
-  open sauce
   https://github.com/oliverborner
 
 */
@@ -48,12 +48,19 @@
 
 // LEDS
 
-int all_letters[] = { 9, 10 };  // PWM, 9 -> D, 10 -> E
-int letterCount = 2; // how many letters you want to drive
+int all_letters[] = { 9, 10, 11, 3, 5, 6 };  // PWM, 9 -> D, 10 -> E, 11 -> C, 3 -> R, 5 ->0, 6 -> W
+int letterCount = 6; // how many letters you want to drive
 
 
-int toggleSwitch1 = 7;
+// initialize toggle switches and their state
+
+int toggleSwitch1 = 7;   // digital pin, has internal pull up resistor
 int toggleSwitchState1 = 0;  
+
+int toggleSwitch2 = 8;   // digital pin, has internal pull up resistor
+int toggleSwitchState2= 0;  
+
+
 
 // OTHER
 
@@ -78,8 +85,9 @@ void setup() {
   // SOUNDSENSOR
   pinMode(soundSensor, INPUT);
 
-  // ToggleSwitch 1
+  // TOGGLESWITCH 1 & 2
   pinMode(toggleSwitch1, INPUT_PULLUP);
+  pinMode(toggleSwitch2, INPUT_PULLUP);
 
 }
 
@@ -88,21 +96,54 @@ void setup() {
 
 void loop() {
   
-  // switch case?
-
   // Toggle Switch 1
   toggleSwitchState1 = digitalRead(toggleSwitch1);
-  
-  if (toggleSwitchState1 == HIGH) {
-    //
+  toggleSwitchState2 = digitalRead(toggleSwitch2);
+
+
+  if (toggleSwitchState1 == HIGH && toggleSwitchState2 == HIGH) {
+    // Default State all LED's turned on
     dec_just_on(all_letters);
-    
-  } else {
-    // 
-    dec_disco(all_letters[0], all_letters[1]);
-    
+  }
+  
+  if (toggleSwitchState1 == LOW && toggleSwitchState2 == HIGH) {
+    // Disco Mode
+    dec_disco(all_letters[0], all_letters[1], all_letters[2], all_letters[3], all_letters[4], all_letters[5]);
+  }
+  
+  if (toggleSwitchState2 == LOW && toggleSwitchState1 == HIGH) {
+    // Sound Recognition
+    dec_sound_recognition(all_letters);
   }
 
+  if (toggleSwitchState1 == LOW && toggleSwitchState2 == LOW) {
+    // Dimming Mode
+ 
+    brightness = 0;
+    fadeAmount = 5;
+    
+    for (int i = 0; i <= 30; i++) {
+      analogWrite(all_letters[0], brightness);
+      analogWrite(all_letters[1], brightness);
+      analogWrite(all_letters[2], brightness);
+      analogWrite(all_letters[3], brightness);
+      analogWrite(all_letters[4], brightness);
+      analogWrite(all_letters[5], brightness);
+        
+      // change the brightness for next time through the loop:
+      brightness = brightness + fadeAmount;
+    
+      // reverse the direction of the fading at the ends of the fade:
+      if (brightness <= 0 || brightness >= 255) {
+        fadeAmount = -fadeAmount;
+      }
+      // wait for 30 milliseconds to see the dimming effect
+      delay(30);
+      
+    }
+    delay(1000);
+
+  }
   
   // STARTUP
   // if no toggleswitch on
@@ -141,7 +182,7 @@ void dec_just_on(int leds[]) {
 
 // DISCO
 
-void dec_disco(int led1, int led2) {
+void dec_disco(int led1, int led2, int led3, int led4, int led5, int led6) {
   
   // Blink 10 times
   
@@ -151,6 +192,10 @@ void dec_disco(int led1, int led2) {
     j = j + 30;
     dec_blink(led1, j);
     dec_blink(led2, j); 
+    dec_blink(led3, j); 
+    dec_blink(led4, j); 
+    dec_blink(led5, j); 
+    dec_blink(led6, j); 
   }
 
   
@@ -159,9 +204,17 @@ void dec_disco(int led1, int led2) {
   for (int i = 0; i <= 3; i++) {
     digitalWrite(led1, HIGH);
     digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);
     delay(500);
     digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
     delay(500);
   }
 
@@ -173,6 +226,10 @@ void dec_disco(int led1, int led2) {
   for (int i = 0; i <= 30; i++) {
     analogWrite(led1, brightness);
     analogWrite(led2, brightness);
+    analogWrite(led3, brightness);
+    analogWrite(led4, brightness);
+    analogWrite(led5, brightness);
+    analogWrite(led6, brightness);
       
     // change the brightness for next time through the loop:
     brightness = brightness + fadeAmount;
@@ -191,6 +248,10 @@ void dec_disco(int led1, int led2) {
   // turn off
   dec_off(led1);
   dec_off(led2); 
+  dec_off(led3); 
+  dec_off(led4);
+  dec_off(led5); 
+  dec_off(led6); 
   delay(1000);
   
   
@@ -201,6 +262,10 @@ void dec_disco(int led1, int led2) {
   for (int i = 0; i <= 30; i++) {
     analogWrite(led1, brightness);
     analogWrite(led2, brightness);
+    analogWrite(led3, brightness);
+    analogWrite(led4, brightness);
+    analogWrite(led5, brightness);
+    analogWrite(led6, brightness);
       
     // change the brightness for next time through the loop:
     brightness = brightness + fadeAmount;
@@ -219,6 +284,10 @@ void dec_disco(int led1, int led2) {
   // turn off
   dec_off(led1);
   dec_off(led2); 
+  dec_off(led3); 
+  dec_off(led4);
+  dec_off(led5); 
+  dec_off(led6); 
   delay(1000);
 
 
@@ -226,9 +295,17 @@ void dec_disco(int led1, int led2) {
   for (int i = 0; i <= 3; i++) {
     digitalWrite(led1, HIGH);
     digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);
     delay(1000);
     digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
     delay(1000);
   }
 
@@ -236,17 +313,74 @@ void dec_disco(int led1, int led2) {
   // turn off
   dec_off(led1);
   dec_off(led2); 
+  dec_off(led3); 
+  dec_off(led4);
+  dec_off(led5); 
+  dec_off(led6); 
   delay(1000);
 
 
   // DE - CROW
   dec_on(led1); 
-  delay(1000);
   dec_on(led2); 
   delay(1000);
+  dec_on(led3);
+  dec_on(led4);
+  dec_on(led5);
+  dec_on(led6); 
+  delay(1000);
+
+   // turn off
+  dec_off(led1);
+  dec_off(led2); 
+  dec_off(led3); 
+  dec_off(led4);
+  dec_off(led5); 
+  dec_off(led6); 
+  delay(1000);
+
 
   // D - W  E - O  C - R 
   // 0 - 5  1 - 4  2 - 3 (and reversed)
+  dec_on(led1); 
+  dec_on(led6);
+  delay(500);
+  dec_on(led2); 
+  dec_on(led5);
+  delay(500);
+  dec_on(led3); 
+  dec_on(led4);
+  delay(1000);
+
+  // turn off
+  dec_off(led1);
+  dec_off(led2); 
+  dec_off(led3); 
+  dec_off(led4);
+  dec_off(led5); 
+  dec_off(led6); 
+  delay(600);
+
+  // reverse
+  dec_on(led3); 
+  dec_on(led4);
+  delay(500);
+  dec_on(led2); 
+  dec_on(led5);
+  delay(500);
+  dec_on(led1); 
+  dec_on(led6);
+  delay(1000);
+
+  // turn off
+  dec_off(led1);
+  dec_off(led2); 
+  dec_off(led3); 
+  dec_off(led4);
+  dec_off(led5); 
+  dec_off(led6); 
+  delay(600);
+
 
   
 }
@@ -254,18 +388,21 @@ void dec_disco(int led1, int led2) {
 
 // SOUND_RECOGNITION
 
-void dec_sound_recognition(int led) {
+void dec_sound_recognition(int leds[]) {
   int statusSensor = digitalRead (soundSensor);
 
   if (statusSensor == 1)
   {
-    digitalWrite(led, HIGH);
-    Serial.write("High");
+     for (int i = 0; i < letterCount; i++) {
+        digitalWrite(leds[i], HIGH);
+     }
+
   }
   else
   {
-    digitalWrite(led, LOW);
-    Serial.write("LOW");
+      for (int i = 0; i < letterCount; i++) {
+        digitalWrite(leds[i], LOW);
+      }
   }
   
 }
